e57890862a6d6759f8bb6c6baef3317c142fe283
frapet <frapet@francescos-MacBook-Pro.local>
2018-05-31 10:47:35 +1000

    base structure set up

---
+++ config.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ * Configuaration file
+ */
+
+//Container for all the enviroments
+var enviroments = {}
+
+enviroments.staging = {
+  'httpport': 3000,
+  'https': 3001,
+  'envName': 'staging'
+}
+
+enviroments.production = {
+  'httpPort': 5000,
+  'httpsPort': 5001,
+  'envName': 'production'
+}
+
+//Determine wich one was passed in the comand line and export the correct enviroment
+var currentEnviroment = typeof (process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : ''
+
+//Check the envirmonet passed exist(default to satging)
+var enviromentToExport = typeof (enviroments[currentEnviroment]) == 'object' ? enviroments[currentEnviroment] : enviroments.staging
+
+//Export the module
+module.exports = enviromentToExport
---
+++ index.js
@@ -0,0 +1,122 @@
+/*
+ *Primary file of the APi
+ *
+ *
+ */
+
+var http = require('http')
+var https = require('https')
+var fs = require('fs')
+var url = require('url')
+var StringDecoder = require('string_decoder').StringDecoder
+var config = require('./config')
+
+
+//Instantiating the HTTP server
+var httpServer = http.createServer(function (req, res) {
+  unifiedServer(req, res)
+});
+
+// Start HTTP server
+httpServer.listen(config.httpPort, function () {
+  console.log(`the server is listening on port ${config.httpPort} in config enviroment ${config.envName}`)
+});
+
+//Instantiating HTTPS server
+var httpsServerOption = {
+  'key': fs.readFileSync('./https/key.pem'),
+  'cert': fs.readFileSync('./https/cert.pem')
+}
+
+var httpsServer = https.createServer(httpsServerOption, function (req, res) {
+  unifiedServer(req, res)
+});
+
+//Start HTTPS server
+httpsServer.listen(config.httpsPort, function () {
+  console.log(`the server is listening on port ${config.httpsPort} in config enviroment ${config.envName}`)
+});
+
+//Define Handlers
+var handlers = {}
+
+//Smaple handler
+handlers.sample = (data, callback) => {
+  //Callback a hhtp status code, and a payload object
+  callback(406, {
+    'name': 'sample handler'
+  })
+}
+
+//Smaple handler
+handlers.notFound = (data, callback) => {
+  callback(404)
+}
+
+//Define a request router
+var router = {
+  'sample': handlers.sample
+}
+
+//All the server logic
+var unifiedServer = () => {
+  //Get the URL and parse it
+  var parsedUrl = url.parse(req.url, true)
+
+  //Get the path
+  var path = parsedUrl.pathname
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+  //Get query string as object
+  var queryStringObject = JSON.stringify(parsedUrl.query)
+
+  //Get the HTTP method
+  var method = req.method.toLowerCase()
+
+  // Get headers
+  var headers = JSON.stringify(req.headers)
+
+  //Get the payload if one
+  var decoder = new StringDecoder('utf-8')
+  var buffer = ''
+
+  //As the data stream comes in it gets written and saved to buffer
+  req.on('data', function (data) {
+    buffer += decoder.write(data)
+  })
+  //When the request data stream ends
+  req.on('end', () => {
+    buffer += decoder.end()
+
+    //Chose the handlershould go to or not found
+    var chosendHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound
+
+    //Construct the data object to send to the handler
+    var data = {
+      'trimmedPath': trimmedPath,
+      'queryStringObject': queryStringObject,
+      'method': method,
+      'headers': headers,
+      'payloads': buffer
+    }
+
+    //Route the request to the chosen handler
+    chosendHandler(data, (statusCode, payload) => {
+      //Use the status code called back or default to 200
+      statusCode = typeof (statusCode) == 'numebr' ? statusCode : 200
+      //Use the payload sent by user or deault to empty object
+      payload = typeof (payload) == 'object' ? payload : {}
+
+      //Convert the payload to a string
+      var payloadString = JSON.stringify(payload)
+
+      //Return the response
+      res.setHeader('Content-Type', 'application/json')
+      res.writeHead(statusCode)
+      res.end(payloadString)
+
+      //Send the request path
+      console.log('Returning this response: ' + statusCode, payloadString)
+    })
+  })
+}
